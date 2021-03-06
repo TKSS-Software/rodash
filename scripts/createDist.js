dd58bb7acd3cb40f9630a5781ffda8f4c2c4a8e0
Tyler Smith <tyler@tksssoftware.com>
2021-03-06 11:40:42 -0400

    Prep for builds

---
+++ scripts/createDist.js
@@ -0,0 +1,14 @@
+// WE"LL DO FUN STUFF HERE
+// const fs = require('fs')
+// const mergeFiles = require('merge-files');
+// const glob = require('glob');
+// const concat = require("concat")
+
+
+// glob(__dirname + '/../out/stage/source/rodash/**/*.brs', {}, (err, files)=>{
+//   const outputPath = __dirname + '/../out/stage/source/rodash/dist/rodash.brs';
+//   const inputPathList = files;
+
+//   concat(inputPathList, outputPath)
+//   concat(inputPathList).then(result => console.log(result.replace(/^'import .*\n?/m, '')))
+// })
