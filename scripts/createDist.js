593db9ba8166c83d3d0e094746ae3c551fb776d0
Tyler Smith <tyler@tksssoftware.com>
2021-03-09 16:57:10 -0400

    add .d.bs to workfow

--- scripts/createDist.js
+++ scripts/createDist.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const mergeFiles = require('merge-files');
 const glob = require('glob');
 const concat = require("concat")
+const copyfiles = require('copyfiles');
 
 
 glob(__dirname + '/../.tmp/source/rodash/**/*.brs', {}, (err, files)=>{
@@ -17,7 +18,33 @@ glob(__dirname + '/../.tmp/source/rodash/**/*.brs', {}, (err, files)=>{
       result = result.replace(/^'import.*\n?/mg, '');
 
       fs.writeFile(outputPath, result, 'utf8', function (err) {
-         if (err) return console.log(err);
+        if (err) return console.log(err);
+
+        glob(__dirname + '/../.tmp/source/rodash/**/*.d.bs', {}, (err, files) => {
+          files.forEach((path) => {
+            var fileName = path.split("/").pop();
+            if (!fileName.includes("_")) {
+              fs.readFile(path, 'utf8', function (err,data) {
+                if (err) {
+                  return console.log(err);
+                }
+                var result = data
+                var hasInternal = result.includes("namespace rodash.internal")
+                console.log("hasInternal", hasInternal)
+                
+                if (hasInternal) {
+                  result = result.replace("namespace rodash.", 'namespace ');
+                } else {
+                  result = result.replace(/^.*namespace.*$/gm, '');
+                }
+                fs.writeFile(outputDirectory + fileName, result, 'utf8', function (err) {
+                  if (err) return console.log(err);
+                  console.log(fileName + ' was copied to ' + fileName);
+                });
+              });
+            }
+          });
+        })
       });
     })
   });
